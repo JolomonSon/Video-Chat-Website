582faf36c6c78967c2c01e786e81d7ccd219c68d
John Solomon <jolomonson@gmail.com>
2022-11-08 14:17:18 +0100

    Completed Token Generation

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.lobby, name='lobby'),
     path('room/', views.room, name='room'),
+
+    path('get_token/', views.getToken)
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from agora_token_builder import RtcTokenBuilder
+import random
+import time
 # Create your views here.
+def getToken(request):
+    appId = "ea5472b2c2ad450397a718fa2e13d4e9"
+    appCertificate = "3f1bd0b48c364a3184955a184428c0d2"
+    channelName = request.GET.get('channel')
+    uid = random.randint(1,230)
+    role = 1 # - default is 1 for admin
+    expirationTimeInSeconds = 3600 * 24
+    currentTimeStamp = time.time()
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+    return JsonResponse({'token':token, 'uid':uid}, safe=False)
 
 def lobby(request):
     return render(request, 'base/lobby.html')
